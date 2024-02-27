b8dede00ecfe0424a13005d550a838f764b1526f
phbrg <xomaicrocefalia123@gmail.com>
2024-02-27 15:27:26 -0300

    fix no send log page to github

---
+++ frontend/src/pages/Logs/Logs.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+
+import { Card } from '../../components/Card/Card';
+
+import useApi from '../../hooks/useApi';
+
+import './Logs.css'
+
+export const Logs = () => {
+  const token = Cookies.get('token') || null;
+  if (!token) {
+    window.location.href = '/login';
+  }
+
+  const { data: userGet, error: userError, loading: userLoading, fetchData: userFetch } = useApi(`${import.meta.env.VITE_API_URL}/personal`, 'GET');
+  const [user, setUser] = useState(null);
+  const { data: logsGet, error: logsError, loading: logsLoading, fetchData: logsFetch } = useApi(`${import.meta.env.VITE_API_URL}/admin/logs`, 'GET');
+  const [logs, setLogs] = useState(null);
+
+  useEffect(() => {
+    if(!user) userFetch();
+    if(!logs) logsFetch();
+  }, [])
+
+  useEffect(() => {
+    if(userGet && user == null) setUser(userGet);
+    if(logsGet && logs == null) setLogs(logsGet);
+    if(userGet) {
+      if(userGet.role == 'BOSS' || userGet.role == 'MANAGER') {
+      } else {
+        window.location.href = '/error'
+      }
+    }
+
+    if(logsGet) {
+      console.log(logsGet);
+    }
+  }, [userGet, logsGet]);
+
+  return (
+    <section className='Logs'>
+      <h1>Logs</h1>
+      {
+        logs && logs.map((log, key) => (
+          <Card key={key} elements={[
+            `ID: ${log.id}`,
+            `Category: ${log.category}`,
+            log.message,
+            `${new Date(log.createdAt).toLocaleDateString()}`
+          ]}/>
+        ))
+      }
+    </section>
+  )
+}
